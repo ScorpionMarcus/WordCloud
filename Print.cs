db3b701e322769e38305f2eafb67437d17e80745
Marcus LeGault <marcus.legault@scorpion.co>
2022-11-11 15:34:56 -0800

    project setup
    generated visual studio console app

---
+++ Print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCloud
+{
+    class Print
+    {
+        public static void parseWords(string word)
+        {
+            List<string> wordList = new List<string>();
+
+            var punctuation = word.Where(Char.IsPunctuation).Distinct().ToArray();
+
+            var words = word.Split().Select(x => x.Trim(punctuation));
+
+            foreach (var i in words)
+            {
+                Console.WriteLine(i);
+                wordList.Add(i);
+            }
+
+            Console.WriteLine(wordList);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace WordCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph1 = "At Scorpion, we have the technology to connect all of your marketing and show you the results you expect from the money you spend. From our research and customer management software to our artificial intelligence-led search engine products, we have the latest technology to grow your business.";
+            string paragraph2 = "Once the technology is in place, our team of specialists will partner with you along the way to handle all of the marketing for your business, so you can focus on providing a great experience to your customers.";
+
+            Print.parseWords(paragraph1);
+            Print.parseWords(paragraph2);
+        }
+    }
+}
